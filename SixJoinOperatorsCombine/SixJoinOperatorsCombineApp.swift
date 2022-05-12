83224d350a708057ac979494f68dafb3b342aa71
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-12 21:09:16 +0500

    Initial Commit

---
+++ SixJoinOperatorsCombine/SixJoinOperatorsCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixJoinOperatorsCombineApp.swift
+//  SixJoinOperatorsCombine
+//
+//  Created by Вячеслав Квашнин on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SixJoinOperatorsCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
